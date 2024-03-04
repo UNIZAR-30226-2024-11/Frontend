da5bf71b69ae661136bdb05651cb5324301551c1
Zentarus <852646@unizar.es>
2024-03-04 21:46:44 +0100

    Pruebas con redirecciones y creación de pantalla de portada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { PortadaComponent } from './portada/portada.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'portada', pathMatch: 'full' },
+    { path: 'inicio', component: InicioComponent },
+    { path: 'portada', component: PortadaComponent }
+];
---
+++ src/app/portada/portada.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-portada',
+  standalone: true,
+  imports: [],
+  templateUrl: './portada.component.html',
+  styleUrl: './portada.component.css'
+})
+
+export class PortadaComponent {
+}
+
