5a0806bf7cfade378a5c766396f5df95498aa0a3
Zentarus <852646@unizar.es>
2024-03-07 23:18:28 +0100

    Odio angular, solucionado problemas en redireccion de componentes. Se ha optado por usar enrutador por hash en vez de html5 para facilitar recarga de paginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { InicioComponent } from './inicio/inicio.component';
 import { PortadaComponent } from './portada/portada.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'portada', pathMatch: 'full' },
     { path: 'inicio', component: InicioComponent },
-    { path: 'portada', component: PortadaComponent }
+    { path: 'portada', component: PortadaComponent },
+    { path: '', redirectTo: 'portada', pathMatch: 'full' }
 ];
--- src/app/portada/portada.component.ts
+++ src/app/portada/portada.component.ts
@@ -5,15 +5,82 @@ import { Router } from '@angular/router';
   selector: 'app-portada',
   standalone: true,
   imports: [],
-  templateUrl: './portada.component.html',
-  styleUrl: './portada.component.css'
+  template: `
+    <div class="container">
+      <div class="fade-in">
+        <img src="assets/UnoGraham_LOGO.png" alt="Logo Uno Graham" class="logo">
+        <button class="button" (click)="redireccionarALaInicio()">Acceder</button>
+      </div>
+    </div>
+  `,
+  styles: [`
+    .container {
+        background-color: #ffe9f8; 
+        background: linear-gradient(to bottom, #f0a3d7, #ffe9f8);
+        display: flex;
+        padding: 0;
+        margin: 0;
+        overflow: hidden; 
+        justify-content: center;
+        align-items: center;
+        height: 98vh;
+    }
+
+    .fade-in {
+        opacity: 0;
+        animation: fadeIn 3s ease forwards;
+        color: #300b24;
+    }
+
+    .logo {
+        width: 1000px; 
+        height: auto; 
+        transform: translate(-45%, -50%); 
+        position: absolute;
+        top: 275px;
+    }
+
+    @keyframes fadeIn {
+        0% {
+            opacity: 0;
+            transform: scale(0.2);
+        }
+        100% {
+            opacity: 1;
+            transform: scale(1);
+        }
+    }
+
+    .button {
+        opacity: 0;
+        animation: fadeIn 3s ease forwards;
+        background-color: #771d55; /* Color de fondo del botón */
+        margin-top: 500px;
+        border: none;
+        color: white;
+        padding: 15px 32px;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        cursor: pointer;
+        border-radius: 10px;
+    }
+
+    h1 {
+        transition: font-size 4s ease; 
+    }
+
+    .fade-in h1 {
+        font-size: 6em; 
+    }
+    `
+]
 })
 
 export class PortadaComponent { 
   constructor(private router: Router) {}
 
   redireccionarALaInicio() {
-    this.router.navigate(['/inicio']); 
+    this.router.navigate(['inicio']); 
   }
 }
-
