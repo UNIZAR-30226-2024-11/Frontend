61ae434c0a939543e84acc6c0ae63690e59bf0db
Zentarus <852646@unizar.es>
2024-03-08 22:38:18 +0100

    Intentando unir backend de login con la pantalla

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 import { PortadaComponent } from './portada/portada.component';
 import { LoginComponent } from './login/login.component';
-import { RegistroComponent } from './registro/registro.component';
+import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
     { path: 'inicio', component: InicioComponent },
     { path: 'portada', component: PortadaComponent },
     { path: 'login', component: LoginComponent},
-    { path: 'registro', component: RegistroComponent},
+    { path: 'register', component: RegisterComponent},
     { path: '', redirectTo: 'portada', pathMatch: 'full' }
 ];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,46 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
+
+interface LoginResponse {
+  error?: string;
+  token?: string;
+}
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  usuario: string = '';
+  password: string = '';
+  error: string = '';
+  /*
+  constructor(private http: HttpClient,
+              private router: Router) {}
+ 
+  login(): void {
+    this.http.post<LoginResponse>('/api/login', { email: this.usuario, password: this.password })
+      .pipe(
+        catchError(error => {
+          console.error('Error de inicio de sesión:', error);
+          this.error = 'Error de inicio de sesión';
+          return throwError(() => new Error(error)); 
+        })
+      )
+      .subscribe(response => {
+        if (response.error) {
+          this.error = response.error;
+        } else if (response.token) {
+          localStorage.setItem('token', response.token);
+          this.router.navigate(['home']);
+        }
+      });
+  }
+  */
 }
