d34f2d8b434972630211a15d6f1a4eea2e1503f1
Zentarus <852646@unizar.es>
2024-03-13 13:56:49 +0100

    Se ha eliminado el boton ajustes

--- src/app/profile/level/level.component.ts
+++ src/app/profile/level/level.component.ts
@@ -9,16 +9,17 @@ import { Component } from '@angular/core';
 })
 
 export class LevelComponent {
+  max_largo_barra = 140; // longitud maxima de la barra de exp en pixeles
   nivel: number = 0; // Esto se igualará al nivel del usuario
   valorProgreso: number = 0; // Esto se igualará a la XP del usuario
 
   ajustarLongitudBarra(valor: number) {
     // 150 es el número máximo de pixeles que tiene la barra de progreso
-    if(valor < 150 - this.valorProgreso){
+    if(valor < this.max_largo_barra - this.valorProgreso){
       this.valorProgreso = this.valorProgreso + valor;
     }
     else {
-      this.valorProgreso = valor - (150 - this.valorProgreso);
+      this.valorProgreso = valor - (this.max_largo_barra - this.valorProgreso);
       this.nivel = this.nivel + 1;
     }
   }
