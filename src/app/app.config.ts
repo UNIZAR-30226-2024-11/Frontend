ea74cc9f71a4b3be58dedfc73b5a66f1c034b952
Zentarus <852646@unizar.es>
2024-04-05 20:02:26 +0200

    Añadido ngx-toastr y animations, creado aviso portapapeles

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,12 @@ import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideToastr } from 'ngx-toastr';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withHashLocation()), 
+  providers: [provideAnimations(),
+              provideToastr({timeOut: 2000, preventDuplicates: true}),
+              provideRouter(routes, withHashLocation()), 
               provideClientHydration(), provideAnimationsAsync()]
 };
--- src/app/crear-partida/crear-partida.component.ts
+++ src/app/crear-partida/crear-partida.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Clipboard} from '@angular/cdk/clipboard'
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-crear-partida',
@@ -8,13 +9,17 @@ import { Clipboard} from '@angular/cdk/clipboard'
   templateUrl: './crear-partida.component.html',
   styleUrl: './crear-partida.component.css'
 })
+
+
 export class CrearPartidaComponent {
   codigoSala: string = '145334'; // Se deberá generar aleatoriamente??
   numJugadores: number = 0;
 
-  constructor(private clipboard: Clipboard) {}
+  constructor(private clipboard: Clipboard,
+              private toastr: ToastrService) {}
 
   copiarAlPortapapeles(codigo: string) {
     this.clipboard.copy(codigo);
+    this.toastr.success('Copiado al portapapeles', 'CÓDIGO DE SALA');
   }
 }
