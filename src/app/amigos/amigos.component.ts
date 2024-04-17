5fa58c3d67823b1a2edd8b9b1cfa3bd1e405dab5
Pablocizq <831754@unizar.es>
2024-04-17 13:20:52 +0200

    Flujo hacia home desde pantalla amigos y lógica de buscar amigos

--- src/app/amigos/amigos.component.ts
+++ src/app/amigos/amigos.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { FlujoComponent } from '../shop/flujo/flujo.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-amigos',
   standalone: true,
-  imports: [],
+  imports: [FlujoComponent, FormsModule, CommonModule],
   templateUrl: './amigos.component.html',
   styleUrl: './amigos.component.css'
 })
 export class AmigosComponent {
+  nombreUsuario: string = '';
+  nombres: string[] = ['Juan', 'María', 'Pedro'];
+  respuesta: string = '\n';
+  usuarioCorrecto: string = '';
+  noEncontrado: string = 'No se ha encontrado el usuario';
+  mensaje: string = '';
+
+  verificarNombre() {
+    // Verificar si el nombre introducido por el usuario está en la lista de nombres
+    if (this.nombres.includes(this.nombreUsuario)) {
+      this.usuarioCorrecto = this.nombreUsuario
+      this.respuesta= this.nombreUsuario
+    } else {
+      this.usuarioCorrecto = this.nombreUsuario
+      this.mensaje = this.noEncontrado + ' ' + this.usuarioCorrecto;
+      this.respuesta = this.mensaje // Ocultar el panel si no se encuentra el nombre
+    }
+  }
 
 }
