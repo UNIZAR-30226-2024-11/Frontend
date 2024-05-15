193ae3b2d662252d8c35d1f79cfe5a0bbfcd71b8
Pablocizq <831754@unizar.es>
2024-05-15 21:08:23 +0200

    Pausa y tablero

--- src/app/tablero/pausa/pausa.component.ts
+++ src/app/tablero/pausa/pausa.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { TableroComponent } from '../tablero.component';
 
 @Component({
   selector: 'app-pausa',
   standalone: true,
-  imports: [],
+  imports: [TableroComponent],
   templateUrl: './pausa.component.html',
   styleUrl: './pausa.component.css'
 })
 export class PausaComponent {
+  /**
+   * Constructor del componente PausaComponent.
+   * @param tableroComp Una instancia del componente TableroComponent.
+   */
+  constructor(private tableroComp: TableroComponent) {}
 
+  /**
+   * Método para cerrar la pantalla de pausa del juego.
+   * Llama al método `closePausa` del componente TableroComponent.
+   */
+  closePausa_callback(){
+     this.tableroComp.closePausa();
+  }
 }
--- src/app/tablero/tablero.component.ts
+++ src/app/tablero/tablero.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Clipboard } from '@angular/cdk/clipboard';
+import { ToastrService } from 'ngx-toastr';
+import { MatDialog } from '@angular/material/dialog';
+import { PausaComponent } from './pausa/pausa.component';
 
 @Component({
   selector: 'app-tablero',
   standalone: true,
-  imports: [],
+  imports: [PausaComponent, FormsModule, CommonModule],
   templateUrl: './tablero.component.html',
   styleUrl: './tablero.component.css'
 })
 export class TableroComponent {
+  mensajes: string[] = [];
+  mensaje: string = '';
+  public show_pausa = false;
 
+  constructor(private clipboard: Clipboard, private toastr: ToastrService,
+              private dialogRef: MatDialog) {}
+
+  ngOnInit(): void {}
+          
+  agnadirMensaje(): void {
+    this.mensajes.push(this.mensaje);
+    this.mensaje = '';
+  }
+
+  /**
+   * Método para abrir la pantalla de pausa
+   */
+  openPausa(){
+    this.show_pausa = true;
+  }
+
+  /**
+   * Método para cerrar la pantalla de pausa
+   */
+  closePausa(){
+    this.show_pausa = false;
+  }
 }
