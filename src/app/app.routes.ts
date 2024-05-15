9d6123f7d3b13b51003148805da0e9481303cba5
Pablocizq <831754@unizar.es>
2024-05-15 21:01:20 +0200

    Ruta tablero

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,7 @@ import { ShopComponent } from './shop/shop.component';
 import { CargaComponent } from './carga/carga.component';
 import { CrearPartidaComponent } from './crear-partida/crear-partida.component';
 import { AmigosComponent } from './amigos/amigos.component';
+import { TableroComponent } from './tablero/tablero.component';
 import { UnirsePartidaComponent } from './unirse-partida/unirse-partida.component';
 
 export const routes: Routes = [
@@ -24,5 +25,6 @@ export const routes: Routes = [
     { path: 'amigos', component: AmigosComponent},
     { path: 'carga', component: CargaComponent},
     { path: 'unirse-partida', component: UnirsePartidaComponent},
+    { path: 'tablero', component: TableroComponent},
     { path: '', redirectTo: 'portada', pathMatch: 'full' }
 ];
