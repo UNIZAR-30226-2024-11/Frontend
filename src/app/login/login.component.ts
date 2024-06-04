4c681db769ad592529999ed7b13a8a9d2c3960d9
Zentarus <852646@unizar.es>
2024-06-04 16:45:48 +0200

    Comunicacion con backend datos de usuario

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,21 +1,16 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import { catchError, throwError } from 'rxjs';
-import { CookieService } from 'ngx-cookie-service';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { CookieService } from 'ngx-cookie-service';
 
-
-/**
- * Componente para la página de inicio de sesión.
- */
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [FormsModule, HttpClientModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
 
@@ -24,16 +19,8 @@ export class LoginComponent {
   passwordMatch: boolean = true;
   private apiURL = 'https://backend-eg2q.onrender.com/api/';
 
-  constructor(private router: Router,
-    private cookieService: CookieService,
-    private http: HttpClient) { }
+  constructor(private router: Router, private http: HttpClient, private cookieService: CookieService) { }
 
-  /**
-   * Método para realizar el inicio de sesión.
-   * Hace una solicitud HTTP POST al servidor de backend para autenticar al usuario.
-   * Si la autenticación es exitosa, guarda el token de autenticación en una cookie y redirige al usuario a la página de inicio.
-   * En caso de error, maneja los errores y actualiza el estado de la contraseña coincidente.
-   */
   login() {
     console.log("Iniciar sesión con correo:", this.correo, " y contraseña:", this.password);
 
@@ -43,16 +30,20 @@ export class LoginComponent {
           if (response.token) {
             // Autenticación válida
             console.log("Token de respuesta recibido correctamente");
+            console.log("ID recibido: ", response.id);
             this.cookieService.set('token', response.token);
+
+            // Guardar información del usuario en localStorage
+            localStorage.setItem('id', response.id);
+            localStorage.setItem('user', JSON.stringify(this.correo));
             this.router.navigate(['/home']);
           } else {
-            // Error de autenticación
+            // Error de autenticacion
             console.error('Error en la autenticación del usuario', response.error);
           }
         },
         error: (error) => {
           console.error('Error en la solicitud HTTP:', error);
-          // Habrá que diferenciar que casos de error devuelve. 
           this.passwordMatch = false;
         }
       });
--- src/app/profile/info-jugador/info-jugador.component.ts
+++ src/app/profile/info-jugador/info-jugador.component.ts
@@ -1,14 +1,66 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule, HttpParams } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-info-jugador',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './info-jugador.component.html',
-  styleUrl: './info-jugador.component.css'
+  styleUrls: ['./info-jugador.component.css']
 })
+export class InfoJugadorComponent implements OnInit {
+  user: string = "";
+  _id?: number;
+  _username?: string;
+  _email?: string;
+  _avatar?: string;
+  _level?: number;
+  _games_won?: number;
+  _games_played?: number;
+  _coins?: number;
+  private apiURL = 'https://backend-eg2q.onrender.com/api';
 
-export class InfoJugadorComponent {
-  nGanadas: number = 0; //Numero de partidas ganadas del usuario
-  nJugadas: number = 0; //Numero de partidas jugadas del usuario
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpClient
+  ) {}
+
+  ngOnInit() {
+    // Obtenemos el usuario
+    const userJson = localStorage.getItem('user');
+    if (userJson) {
+      this.user = JSON.parse(userJson);
+    }
+    const idJson = localStorage.getItem('id');
+
+    this.http.get<any>(`${this.apiURL}/userdata/${idJson}`).subscribe({
+      next: (userData) => {
+        console.log('Datos del usuario recibidos correctamente');
+        this._coins = userData.coins || 0;
+        this._games_won = userData.games_won || 0;
+        this._username = userData.username;
+        this._email = userData.email;
+        // Faltar asignar nJugadas aquí 
+      },
+      error: (error) => {
+        console.error('Error al obtener los datos del usuario:', error);
+      }
+    });
+  }
 }
+
+/*
+  POSIBLES VALORES QUE SE PUEDE RECIBIR
+  DEL BACKEND SOBRE EL USUARIO:
+  -----------------------------------------
+  id?: number;
+  username?: string;
+  email?: string;
+  avatar?: string;
+  level?: number;
+  games_won?: number;
+  coins?: number;
+*/
